32d2927f74257e1bc1f2b7c8d1cb831470e9e806
articulite <kaikau@gmail.com>
2025-03-15 02:28:36 -0700

    add bat for self signed cert creation

---
+++ create_self_signed_certs.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Windows batch file to create self-signed certificates
+REM Requires OpenSSL to be installed and in the PATH
+
+REM Create certs directory if it doesn't exist
+if not exist certs mkdir certs
+
+REM Generate private key using secp384r1 curve
+set KEY_FILE=certs\certificate.key
+openssl ecparam -name secp384r1 -genkey -out %KEY_FILE%
+echo Created %KEY_FILE%
+
+REM Create self-signed certificate
+set CERT_FILE=certs\certificate.pem
+openssl req -new -x509 -days 10 -subj "/CN=Test Certificate" -addext "subjectAltName = DNS:localhost" -key %KEY_FILE% -sha384 -out %CERT_FILE%
+echo Created %CERT_FILE%
+
+REM Compute fingerprint
+set FINGERPRINT_FILE=certs\certificate_fingerprint.hex
+openssl x509 -in %CERT_FILE% -outform der | openssl dgst -sha256 -binary > %FINGERPRINT_FILE%
+echo Created %FINGERPRINT_FILE%
+
+echo.
+echo Certificate generation complete.
+pause 
